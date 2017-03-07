afbffcdac7585161a2524ea95f0b34b0d42a062f
Aman Arora <aman.arora@comprotechnologies.com>
2017-03-07 17:20:37 +0530

    Added Static HTML for Header and Footer in StepBuilder Component

--- client/app/shared/dropzone/dropzone.component.ts
+++ client/app/shared/dropzone/dropzone.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { BaseComponent } from '../base.component';
+import { itemSchema } from '../UIConfig.model';
 
 declare var Dropzone: any;
 Dropzone.autoDiscover = false;
@@ -10,10 +11,13 @@ Dropzone.autoDiscover = false;
 })
 export class DropzoneComponent extends BaseComponent implements OnInit {
   @ViewChild('dropzone') dropzoneContainer;
+  labelConfig: itemSchema = new itemSchema();
   constructor(private elementRef: ElementRef){
     super();
   }
   ngOnInit() {
+    this.labelConfig.rendererProperties.text = this.compConfig.label;
+    this.labelConfig.rendererProperties.type = 'ElementHeading';
     let dropzone = new Dropzone(this.dropzoneContainer.nativeElement, {
       url: "/api/file",
       dictDefaultMessage: this.compConfig.rendererProperties.placeHolder
