7b3cefeec3ab3c4b30f315a757b67af72d4670e6
ravicompro <ravi.sahu@comprotechnologies.com>
2017-03-08 11:55:17 +0530

    adding code for db api call.

--- server/controllers/databaseFileStoreManager.js
+++ server/controllers/databaseFileStoreManager.js
@@ -3,11 +3,11 @@ const fal = require('./fileAccessLayerController');
 
 module.exports.getUIConfig = function(templateId, callback1) {
     
-    let uiconfig; 
+    let absolutePath; 
     dal.getUIConfigPath(templateId, (data, error) => {
         if(!error && data.success) {
-            uiconfig = fal.getUIConfig(data[0].ui_config_path);
-            callback1(uiconfig);
+            absolutePath = fal.getUIConfig(data[0].ui_config_path);
+            callback1(absolutePath);
         }
     });
 };
--- server/controllers/fileAccessLayerController.js
+++ server/controllers/fileAccessLayerController.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const config = require('../config/config');
 
 module.exports.getUIConfig = function(filepath) {
-    let absolutePath = path.join(__dirname, "../" + filepath);
+    let absolutePath = config.root + "/" + filepath;
     return absolutePath;
 };
