070fbd193a3a83153cb680e72b16e2d2c88eba39
prateekbhutani <prateek.bhutani@comprotechnologies.com>
2017-03-10 17:15:05 +0530

    Moving property accessing to getNestedObject function
    Regression not expected.

--- client/app/step-builder/step-input-area/skill-manager.service.ts
+++ client/app/step-builder/step-input-area/skill-manager.service.ts
@@ -4,31 +4,21 @@ import { Injectable } from '@angular/core';
 export class SkillManagerService {
   stepData: any;
   constructor() {
+    //Temporary Variable To Store Model Reference - To be replaced with Obtained Model Reference
     this.stepData = { "views": { "1": { "documentTitle": "", "documentData": { "name": "", "path": "" }, "sheets": [{ "name": "", "path": "", "gridImage": { "name": "img.png" }, "rowImage": { "name": "" }, "columnImage": { "name": "" }, "cellImage": { "name": "" } }], "taskbarPreviewImage": { "name": "", "path": "" } }, "2": { "sheetInAction": "", "sourceRange": "", "destinationRange": "" }, "3": { "workbookData": { "name": "", "path": "" }, "sheets": [{ "name": "", "path": "", "gridImage": { "name": "" }, "rowImage": { "name": "" }, "columnImage": { "name": "" }, "cellImage": { "name": "" } }] } } };
   }
 
   getSheetNameAndSheetCountFromInitDocJSON(initDocJSON, modelReference) {
 
-    var mRArr = modelReference.split('.');
-    var dependantSheetArrayInModel = [];
-
     //Extract Array of the Dependant Sheets
-    for (let nestingLevel = 0; nestingLevel < mRArr.length; nestingLevel++) {
-      if (dependantSheetArrayInModel.length === 0) {
-        dependantSheetArrayInModel = this.stepData[mRArr[nestingLevel]];
-      }
-      else {
-        dependantSheetArrayInModel = dependantSheetArrayInModel[mRArr[nestingLevel]];
-      }
-    }
-
+    var dependantSheetArrayInModel = this.getNestedObject(this.stepData, modelReference);
     //Add The Required Number of Sheets in Model
     if (initDocJSON.sheetCount >= dependantSheetArrayInModel.length) {
       let sheetCountDiff = initDocJSON.sheetCount - dependantSheetArrayInModel.length;
       while (sheetCountDiff > 0) {
         dependantSheetArrayInModel.push(JSON.parse(JSON.stringify(dependantSheetArrayInModel[(dependantSheetArrayInModel.length - 1)])));
         sheetCountDiff--;
-		
+
       }
     }
 
@@ -36,10 +26,19 @@ export class SkillManagerService {
     for (let sheetNum = 0; sheetNum < initDocJSON.sheetCount; sheetNum++) {
       dependantSheetArrayInModel[sheetNum].name = initDocJSON.sheets[sheetNum].name;
     }
-    console.log('Updated - Model : ',this.stepData);
+    console.log('Updated - Model : ', this.stepData);
   }
 
-  addSheetNamesToDropdown(initDocJSON, modelReference){
+  getNestedObject(inputObject, propertyAccessorPath) {
+    var accessedObject = inputObject;
+    var propertyAccessorPathArray = propertyAccessorPath.split('.');
+    for (let nestingLevel = 0; nestingLevel < propertyAccessorPathArray.length; nestingLevel++) {
+      accessedObject = accessedObject[propertyAccessorPathArray[nestingLevel]];
+    }
+    return accessedObject;
+  }
+
+  addSheetNamesToDropdown(initDocJSON, modelReference) {
     console.log('addSheetNamesToDropdown Function');
   }
 
