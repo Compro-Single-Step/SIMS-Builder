9c17c511d0b067e2d1af4081e3fb7d537a2e7504
Punit Gupta <punit.gupta@comprotechnologies.com>
2017-03-15 18:55:41 +0530

    Adding XML Generator to new folder structure.

---
+++ server/modules/skill/xmlGenerationHandler.js
@@ -0,0 +1,72 @@
+const dbFilestoreMgr = require('./dbFilestoreMgr');
+const translator = require("./ioTranslator.js");
+const xmlGenerator = require("./xmlGenerator/Step.js");
+
+
+function identifySkill(templateId){
+    return {
+        "movecellcontent":"MoveCell_Skill"
+    }[templateId];
+}
+
+module.exports.generateStepXML = function(taskId, stepIndex, templateId, callback){
+
+    dbFilestoreMgr.getStepUIState(taskId, stepIndex, (stepUIState, error) => {
+        if(!error){
+            dbFilestoreMgr.getIOMap(templateId, (error, IOMapJson) => {
+                if(!error){
+
+                    //IO Translator
+                    let IOMap = JSON.parse(IOMapJson);
+
+                    var identifiedSkill = identifySkill(templateId);
+                    var skillRef = require("./"+identifiedSkill);
+            
+                    stepUIState = stepUIState[0]._doc.task_data['step_' + stepIndex];
+                    attrValueMap = translator.translateMap(IOMap, stepUIState, skillRef);
+
+                    //XML GENERATION
+                    dbFilestoreMgr.getSkillXML(templateId, (error, skillTemplate) => {
+                        if(!error){
+                            let newStep = new xmlGenerator(skillTemplate, attrValueMap);
+                            let OutputXML = newStep.stepGenerator();
+                            callback(error, OutputXML);
+                        }
+                        else{
+                            callback(error, skillTemplate);
+                        }
+                    });
+                }
+                else{
+                    callback(error, IOMapJson);
+                }
+            });
+        }
+        else{
+            callback(error, stepUIState);
+        }
+    });
+    
+
+
+//     Promise.all([
+//     dbFilestoreMgr.getSkillXML(templateId),
+//     readFile('./IOMap.json')
+//     ]).then((files) => {
+//         let newStep = new step(...files);
+//         let OutputXML = newStep.stateGenerator();
+//         res.send(OutputXML);
+//         })
+//         .catch((error) => console.log(error))
+
+// function readFile(url){
+//     return promise = new Promise((resolve,reject) => {
+//         fs.readFile(url,"utf8", (err, data) => {
+//             if(err)
+//                 reject(err);
+//             else
+//                 resolve(data);
+//         })
+//     })
+// }
+}
