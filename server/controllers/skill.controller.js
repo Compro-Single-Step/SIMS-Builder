2e9349d1e24941def99bee29fbc3534e3dd06c6d
vagartu <tushar.agarwal@pearson.com>
2017-03-16 18:52:35 +0530

    adding generateXml api handling

--- server/controllers/skill.controller.js
+++ server/controllers/skill.controller.js
@@ -1,5 +1,6 @@
 const uiHandler = require('../modules/skill/uiHandler');
 const dbFilestoreMgr = require('../modules/skill/dbFilestoreMgr');
+const xmlGenHandler = require('../modules/skill/xmlGenerationHandler');
 
 module.exports.getUIConfig = function(templateId, data, callback) {
     return uiHandler.getUIConfig(templateId, data, callback);
@@ -8,3 +9,8 @@ module.exports.getUIConfig = function(templateId, data, callback) {
 module.exports.saveStepUIState = function(taskId, stepIndex, stepUIData, callback) {
     dbFilestoreMgr.saveStepUIState(taskId, stepIndex, stepUIData, callback);
 };
+
+module.exports.generateXML = function(templateId, taskId, stepIdx, callback){
+  xmlGenHandler.generateStepXML(templateId, taskId, stepIdx, callback);
+}
+
