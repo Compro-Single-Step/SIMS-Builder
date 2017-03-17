d46d38273591c03c7d78fe6ea67de4385d4f9cfd
tanujaggarwal <tanuj.aggarwal@comprotechnologies.com>
2017-03-17 16:40:32 +0530

    changes in auth api

--- client/app/auth.module.ts
+++ client/app/auth.module.ts
@@ -3,7 +3,9 @@ import { Http, RequestOptions } from '@angular/http';
 import { AuthHttp, AuthConfig } from 'angular2-jwt';
 
 export function authHttpServiceFactory(http: Http, options: RequestOptions) {
-  return new AuthHttp(new AuthConfig(), http, options);
+  return new AuthHttp(new AuthConfig({
+    noTokenScheme : true
+  }), http, options);
 }
 
 @NgModule({
