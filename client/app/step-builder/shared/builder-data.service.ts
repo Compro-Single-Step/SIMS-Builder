997851bfab941c71091d3d013e9039d84f90833a
Aman Arora <aman.arora@comprotechnologies.com>
2017-03-17 12:27:44 +0530

    Integrate save API with the front end.

--- client/app/step-builder/shared/builder-data.service.ts
+++ client/app/step-builder/shared/builder-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Response } from '@angular/http';
+import { Response, Headers, RequestOptions } from '@angular/http';
 import { HttpClient } from '../../_services/http.client';
 
 import { UIConfig } from '../../shared/UIConfig.model';
@@ -14,11 +14,20 @@ export class BuilderDataService {
   constructor(private httpClient: HttpClient) { }
   getuiconfig(): Observable<UIConfig> {
     return this.httpClient.get('api/skill/uiconfig/movecellcontent')
+    // TODO: Dynamically create the GET URL (api/skill/uiconfig /<SKILL ID>)
                         .map(this.extractData)
                         .catch(this.handleError);
   }
+  saveSkillData (data: Object): Observable<Object> {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
+    // TODO: Dynamically create the post URL (api/skill/taskstep/ <TASK ID> / <STEP NUMBER>)
+    return this.httpClient.post('api/skill/taskstep/task_1/2', { data } , options)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
   private extractData(res: Response) {
-    let body = res.json();
+    const body = res.json();
     return body || { };
   }
   private handleError (error: Response | any) {
