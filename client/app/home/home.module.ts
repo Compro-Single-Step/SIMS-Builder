d5babc8db32c869433ef70ee0f7c64966c18fdd3
chirag-compro <chirag.khanna@comprotechnologies.com>
2017-03-18 11:51:49 +0530

    Update home.module.ts

--- client/app/home/home.module.ts
+++ client/app/home/home.module.ts
@@ -2,7 +2,6 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { FormsModule }  from '@angular/forms';
-import { TooltipModule } from 'ng2-bootstrap';
 
 import { ROUTES }       from './home.routes';
 
