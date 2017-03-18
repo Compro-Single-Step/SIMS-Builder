08795e053b8e8b5c2d684a0f0d22926b66450d4f
Chirag Khanna <Chirag@COMPRO>
2017-03-18 10:46:24 +0530

    Removing extra imports

--- client/app/task-builder/task-builder.component.ts
+++ client/app/task-builder/task-builder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
