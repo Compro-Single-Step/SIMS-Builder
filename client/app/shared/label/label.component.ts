8b6b7eb8e0e1a0358f2800d8227cc7c43ede6e10
ashish.khandelwal <ashish.khandelwal@comprotechnologies.com>
2017-03-20 11:01:31 +0530

    Enhancements in component inheritance.

--- client/app/shared/label/label.component.ts
+++ client/app/shared/label/label.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BaseComponent } from '../base.component';
 
 @Component({
@@ -6,11 +6,12 @@ import { BaseComponent } from '../base.component';
   templateUrl: './label.component.html',
   styleUrls: ['./label.component.scss']
 })
-export class LabelComponent extends BaseComponent implements OnInit {
+export class LabelComponent extends BaseComponent {
   text: string;
   type: string = 'ElementHeading';
 
   ngOnInit() {
+    super.ngOnInit();
     this.text = this.compConfig.rendererProperties.text;
     this.type = this.compConfig.rendererProperties.type;
   }
--- client/app/shared/select/select.component.ts
+++ client/app/shared/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BaseComponent } from '../base.component';
 import { itemSchema } from '../UIConfig.model';
 
@@ -7,11 +7,12 @@ import { itemSchema } from '../UIConfig.model';
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss']
 })
-export class SelectComponent extends BaseComponent implements OnInit {
+export class SelectComponent extends BaseComponent {
   labelConfig: itemSchema = new itemSchema();
   itemList: Array<string>;
 
   ngOnInit() {
+    super.ngOnInit();
     this.labelConfig.rendererProperties.text = this.compConfig.label;
     this.labelConfig.rendererProperties.type = 'ElementHeading';
     //TODO: Bind the data present in the data model at the reference mentioned in the val node of the ui config.
