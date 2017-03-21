9e8e0153f93ba9b09e8ac6b50008f5828d5eeac1
Chirag Khanna <Chirag@COMPRO>
2017-03-21 17:11:49 +0530

    Code for routing to step builder

--- client/app/task-builder/taskstep/taskstep.component.ts
+++ client/app/task-builder/taskstep/taskstep.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'taskbuilder-taskstep',
@@ -8,10 +9,17 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TaskstepComponent implements OnInit {
 @Input() stepData: Array<Object> = [];
 step;
+taskID;
 errorMessage;
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.step=this.stepData;
+    this.route.params.subscribe((params: Params) => {
+      this.taskID = params["id"];
+    })
+   }
+   callStepBuilder(){
+     this.router.navigate(["task",this.taskID,"step",this.step.stepIndex]);
    }
 }
