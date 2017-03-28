5b91381f695fdf57c4c0b17f6b837a030d9f0379
tanujaggarwal <tanuj.aggarwal@comprotechnologies.com>
2017-03-28 18:17:56 +0530

    Adding code for hitting sim5server API for previerw of task.

--- server/routes/taskpreview/archiver.js
+++ server/routes/taskpreview/archiver.js
@@ -1,24 +1,51 @@
 var file_system = require('fs');
+var request = require('request');
 var archiver = require('archiver');
-var dirToCompress = 'D:\\SIMBuilder\\server\\fileStore\\XMLs\\exp\\xl\\01\\01.t1';
+var dirToCompress = 'E:\\SIMS-Builder\\server\\fileStore\\XMLs\\exp\\xl\\01\\01.t1';
+var serverUrl = "sjkfb";
 
 module.exports = function (req, res, next) {
     
 var archive = archiver.create('zip', {});
+
 archive.on('error', function (err) {
     throw err;
 });
 
-    var output = file_system.createWriteStream('/testDir/myZip.zip'); //path to create .zip file
-    output.on('close', function () {
-        console.log(archive.pointer() + ' total bytes');
-        console.log('archiver has been finalized and the output file descriptor has closed.');
-    });
+var output = file_system.createWriteStream('/testDir/myZip.zip'); //path to create .zip file
 
-    archive.pipe(output);
+output.on('close', function () {
+    var req = request.post({
+			url: "http://localhost:4001/ServerCode/SIM5Service.ashx",
+			qs: {
+				Method: "SaveTaskFolder",
+				taskId: 'blog example',
+				taskFolderPath: +new Date(),
+				appID: 'blog example',
+				description: "desc",
+				xmlPath: "task.xml",
+				scenarioType: "T1",
+				etextURL: "",
+				videoURL: "",
+				practiceJSONURL: ""
+			}
+		}, function (err, resp, body) {
+	  		if (err) {
+				console.log('Error!');
+	  		} 
+	  		else {
+	    		console.log('URL: ' + body);
+	  		}
+		}
+	);
+	var form = req.form();
+	form.append('file', file_system.createReadStream('/testDir/myZip.zip'));
+});
 
-    archive.directory(dirToCompress);
+archive.pipe(output);
 
-    archive.finalize();
+archive.directory(dirToCompress);
+
+archive.finalize();
 
 };
