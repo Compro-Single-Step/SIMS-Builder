89269ce1a241f6c7b503075ef2cc4599f5be9bf5
gautambishwas <gautam.bishwas@comprotechnologies.com>
2017-03-29 15:54:03 +0530

    Making Middleware for archiving and transfer

--- server/routes/taskpreview/archiver.js
+++ server/routes/taskpreview/archiver.js
@@ -1,12 +1,11 @@
 var file_system = require('fs');
-var request = require('request');
 var archiver = require('archiver');
 var path = require('path');
 var serverRootPath = path.normalize(__dirname + '/../..');
 
 var sourcePath = '\\fileStore\\XMLs\\exp\\xl\\01\\01.t1';
 
-var serverUrl = "http://localhost:4001/ServerCode/SIM5Service.ashx";
+
 var dirToCompress = serverRootPath + sourcePath;
 
 module.exports = function (req, res, next) {
@@ -20,34 +19,10 @@ archive.on('error', function (err) {
 var output = file_system.createWriteStream(serverRootPath+'/tmp/myZip.zip'); //path to create .zip file
 
 output.on('close', function () {
-    var req = request.post({
-			url: serverUrl,
-			qs: {
-				Method: "SaveTaskFolder",
-				taskId: 'blog example',
-				taskFolderPath: +new Date(),
-				appID: 'blog example',
-				description: "desc",
-				xmlPath: "task.xml",
-				scenarioType: "T1",
-				etextURL: "",
-				videoURL: "",
-				practiceJSONURL: ""
-			}
-		}, function (err, resp, body) {
-	  		if (err) {
-				console.log('Error!');
-	  		} 
-	  		else {
-	    		console.log('URL: ' + body);
-	  		}
-		}
-	);
-	var form = req.form();
-	form.append('file', file_system.createReadStream(serverRootPath+'/tmp/myZip.zip'));
+	next();
 });
 
 archive.pipe(output);
-  archive.directory(dirToCompress, sourcePath);
+archive.directory(dirToCompress, sourcePath);
 archive.finalize();
 };
--- server/routes/taskpreview/taskPreview.js
+++ server/routes/taskpreview/taskPreview.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var sleArchiver = require('./archiver');
-//var transfer = require('./transfer');
+var transfer = require('./transfer');
 
 
 module.exports = function () {
 
     router.get('/',
         sleArchiver,
+        transfer,
        
         function (req, res) {
 			if (req.sleID && req.data) return res.status(200).json(req.data);
