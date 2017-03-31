b69b6d29eb9c88426f6f2a68b4d845534679665d
tanujaggarwal <tanuj.aggarwal@comprotechnologies.com>
2017-03-31 18:42:03 +0530

    Adding further changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var git = require('gulp-git');
 var runSequence = require('run-sequence');
 var clean = require('gulp-clean');
-
+var robocopy = require('robocopy');
+var es         = require('event-stream');
+var jeditor = require('gulp-json-editor');
 
 gulp.task('clean', function () {
     return gulp.src('checkout', {read: false})
@@ -22,4 +24,57 @@ gulp.task('checkoutDevelop', function(){
   });
 });
 
-gulp.task('build')
+gulp.task('cloneRelease', function(){
+  git.clone('https://tanujaggarwal@github.com/Compro-Single-Step/SIMS-Builder.git',
+            {args:'./checkout/qaRelease'}, function(err) {
+    if (err) throw err;
+  });
+});
+
+gulp.task('checkoutRelease', function(){
+  git.checkout('SIM-Builder-Release', {cwd: './checkout/qaRelease'}, function (err) {
+    if (err) throw err;
+  });
+});
+
+var filesToMove = [
+        '/dist',
+        '/package.json'
+    ];
+
+gulp.task('move', function(){
+
+  var distFolder = gulp.src('./checkout/develop/dist/**/*')
+        .pipe(gulp.dest('./checkout/qaRelease/dist'));
+
+  var packageJson = gulp.src('./checkout/develop/package.json')
+      .pipe(jeditor(function(json) {
+        json.scripts.start = "node server/server";
+        return json; // must return JSON object. 
+      }))
+      .pipe(gulp.dest('./checkout/qaRelease/'));
+
+  return es.concat(distFolder, packageJson);
+});
+
+gulp.task('add', function() {
+  return gulp.src('./checkout/qaRelease/')
+    .pipe(git.add());
+});
+
+gulp.task('commit', function() {
+
+    return gulp.src('./checkout/qaRelease/')
+      .pipe(git.commit("initial test"));
+
+});
+
+gulp.task('push', function(){
+  git.push('https://tanujaggarwal@github.com/Compro-Single-Step/SIMS-Builder.git', 'SIM-Builder-Release', function (err) {
+    if (err) throw err;
+  });
+});
+
+gulp.task('gitsend', function() {
+  runSequence('add', 'commit', 'push');
+});
