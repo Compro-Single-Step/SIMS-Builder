28d5bb886e0a78654e112ae6da1d5f2e4b862bac
Chirag Khanna <Chirag@COMPRO>
2017-04-01 17:06:02 +0530

    Adding client as weel as server code for task preview

--- server/routes/taskpreview/archiver.js
+++ server/routes/taskpreview/archiver.js
@@ -2,14 +2,14 @@ var file_system = require('fs');
 var archiver = require('archiver');
 var path = require('path');
 var serverConfig = require('../../config/server.config');
+var config = require('../../config/config');
 
 module.exports = function (req, res, next) {
     
 var archive = archiver.create('zip', {});
 var taskId = req.query.taskId || "GO16.XL.03.3A.02.T1";
 var serverRootPath = path.normalize(__dirname + '/../..');
-var sourcePath = '\\fileStore\\XMLs\\' + taskId;
-var dirToCompress = serverRootPath + sourcePath;
+var dirToCompress = config.fileStore.xmlFolder;
 
 archive.on('error', function (err) {
     throw err;
