a919e81ed4cf92e71c7159bae8a6399129ac5612
tanujaggarwal <tanuj.aggarwal@comprotechnologies.com>
2017-04-01 15:29:35 +0530

    Refactoring some code for preview

--- server/routes/taskpreview/transfer.js
+++ server/routes/taskpreview/transfer.js
@@ -2,31 +2,24 @@ var file_system = require('fs');
 var path = require('path');
 var serverRootPath = path.normalize(__dirname + '/../..');
 var request = require('request');
-var serverUrl = "http://tanuj/sims_server/ServerCode/SIM5Service.ashx";
+var serverConfig = require('../../config/server.config');
+var serverUrl = serverConfig.previewServerURL + "/ServerCode/SIM5Service.ashx";
 
 module.exports = function (req, res, next) {
+var taskId = req.params.taskId || "GO16.XL.03.3A.02.T1";
 var req = request.post({
 			url: serverUrl,
 			qs: {
 				Method: "SaveTaskFolder",
-				taskId: 'GO16.XL.03.3A.02.T1',
-				stepNo: "",
-				taskFolderPath: +new Date(),
-				appID: 'blog example',
-				description: "desc",
-				xmlPath: "task.xml",
-				scenarioType: "T1",
-				etextURL: "",
-				videoURL: "",
-				practiceJSONURL: ""
+				taskId: taskId,
+				stepNo: req.params.stepNo || "",
 			}
 		}, function (err, resp, body) {
 	  		if (err) {
 				console.log('Error!');
 	  		} 
 	  		else {
-	    		console.log('URL: ' + body);
-	    		res.locals.urlToLaunch = body;
+	    		res.locals.urlToLaunch = serverConfig.previewServerURL + "/SIM5Frame.aspx?resLinkID=taskid:" + taskId;
 	    		next();
 	  		}
 		}
