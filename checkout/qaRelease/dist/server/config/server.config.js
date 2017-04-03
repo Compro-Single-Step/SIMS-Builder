10b9907bf3276db99f33354f4c4a00fc8d2d8a19
tanujaggarwal <tanuj.aggarwal@comprotechnologies.com>
2017-04-03 18:19:51 +0530

    removing all files

--- checkout/qaRelease/dist/server/config/server.config.js
+++
@@ -1,8 +0,0 @@
-var server = {
-    SECRET: 'SIMBUILDER_SECRET',
-    TOKEN: {
-    	EXPIRATION_TIME: 60 * 60 // in seconds.
-    }
-}
-
-module.exports = server;
--- checkout/qaRelease/dist/server/modules/skill/uiHandler.js
+++
@@ -1,5 +0,0 @@
-const dbFilestoreMgr = require('./dbFilestoreMgr');
-
-module.exports.getUIConfig = function(templateId, data, callback) {
-    return dbFilestoreMgr.getUIConfig(templateId, callback);
-};
