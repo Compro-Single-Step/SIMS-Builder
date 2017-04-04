1aa08bd045816e36b6fa5b1fb764cc2fff43f7ee
vbishg1 <gautam.bishwas@pearson.com>
2017-04-04 18:02:41 +0530

    1598219 initial test

---
+++ checkout/qaRelease/dist/server/config/server.config.js
@@ -0,0 +1,9 @@
+var server = {
+    SECRET: 'SIMBUILDER_SECRET',
+    previewServerURL: 'http://tanuj/sims_server',
+    TOKEN: {
+    	EXPIRATION_TIME: 60 * 60 // in seconds.
+    }
+}
+
+module.exports = server;
---
+++ checkout/qaRelease/dist/server/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var loginRouter = require('./login/login.route');
+var taskBuilderData = require('./taskBuilderData');
+var skillRoutes = require('./skill.routes');
+var verifyToken = require('./login/verifyToken');
+var taskPreviewRouter = require('./taskpreview/taskPreview');
+
+module.exports = function () {
+	router.use('/login', loginRouter());		
+	
+	router.use('*', verifyToken);
+
+	router.use('/taskPreview', taskPreviewRouter());
+	router.use('/skill', skillRoutes);
+    router.use('/fetchTaskData', taskBuilderData);
+
+
+	return router;
+};
