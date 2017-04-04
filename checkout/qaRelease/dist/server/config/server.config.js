44fea1386b43afac733c383e622c6fdc1765dc56
gautambishwas <gautam.bishwas@comprotechnologies.com>
2017-04-04 17:58:23 +0530

    Rmoving

--- checkout/qaRelease/dist/server/config/server.config.js
+++
@@ -1,9 +0,0 @@
-var server = {
-    SECRET: 'SIMBUILDER_SECRET',
-    previewServerURL: 'http://tanuj/sims_server',
-    TOKEN: {
-    	EXPIRATION_TIME: 60 * 60 // in seconds.
-    }
-}
-
-module.exports = server;
--- checkout/qaRelease/dist/server/routes/login/token.constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	TYPES: {
-		ACCESS_TOKEN: 1,
-		REFRESH_TOKEN: 2
-	}
-};
