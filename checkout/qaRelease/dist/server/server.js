2b354c3afcd22d5f07f3dffd51f8c0d819a9c96a
vbishg1 <gautam.bishwas@pearson.com>
2017-04-05 12:49:40 +0530

    fc9c971 Adding New Module

---
+++ checkout/qaRelease/dist/server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+const config = require('./config/config');
+
+// Get our API routes
+const apiRouter = require('./routes');
+
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '/../dist/client')));
+
+// Set our api routes
+app.use('/api', apiRouter());
+
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../dist/client/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || config.port;
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
