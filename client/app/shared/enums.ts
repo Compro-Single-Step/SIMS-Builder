dfb2efe04add85263fc090112fb00e6a94d4c473
Chirag Khanna <Chirag@COMPRO>
2017-04-06 16:41:40 +0530

    Changes for Favicon. code enhancement for error messages displayed

--- client/app/shared/enums.ts
+++ client/app/shared/enums.ts
@@ -6,7 +6,7 @@ export const LabelTypes =  {
 export const Map={
     'USER_ADDED' : "User Added",
     "DATABASE_ERROR" : "Database Error",
-    "USERNAME/EMAIL_PRESENT" : "'User Name / Email  Already Present",
+    "USERNAME/EMAIL_PRESENT" : "User Name / Email  Already Present",
     "EMAIL_PRESENT" : "Email  Already Present",
     "USERDATA_UPDATED" : "User Data Updated",
     "NO_CHANGES" : "No Changes"
