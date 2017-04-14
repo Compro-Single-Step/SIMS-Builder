b50de82b51c64f4617b1eeb8becfabd9f044502e
vagartu <tushar.agarwal@pearson.com>
2017-04-14 13:24:42 +0530

    Updating indentation

--- server/controllers/filestore.controller.js
+++ server/controllers/filestore.controller.js
@@ -28,65 +28,65 @@ class FileStoreController {
         return config.fileStore.xmlFolder + taskId + "/" + stepIdx + "/";
     }
 
-    getSimsXmlStepFolderPath(taskId, stepIdx){
+    getSimsXmlStepFolderPath(taskId, stepIdx) {
         return this.getTaskFolderPath(taskId, stepIdx) + stepIdx + "/";
     }
 
     copyAssetToTaskFolder(srcPath, taskParams) {
 
-    var filepathArr = srcPath.split("/")
-            var fileName = filepathArr[filepathArr.length - 1].trim();
-            var fileTypeArr = fileName.split(".")
-            var fileType = fileTypeArr[fileTypeArr.length - 1];
-            var resFileType = fileType;
-            if (resTypeMap[fileType]) {
-                resFileType = resTypeMap[fileType]
-            }
-            //path to save the file
-            var absFilePath = this.getFileStoreStepFolderPath(taskParams.taskId, taskParams.stepIndex);
-            
-            //path to return for the file
-            var srcPath = config.fileStore.resourceFolder + srcPath;            
-            var destPath = this.getStepXMLFolderPath(taskParams.taskId, taskParams.stepIndex);
-            var relativeXmlPath = this.getSimsXmlStepFolderPath(taskParams.taskId, taskParams.stepIndex);
+        var filepathArr = srcPath.split("/")
+        var fileName = filepathArr[filepathArr.length - 1].trim();
+        var fileTypeArr = fileName.split(".")
+        var fileType = fileTypeArr[fileTypeArr.length - 1];
+        var resFileType = fileType;
+        if (resTypeMap[fileType]) {
+            resFileType = resTypeMap[fileType]
+        }
+        //path to save the file
+        var absFilePath = this.getFileStoreStepFolderPath(taskParams.taskId, taskParams.stepIndex);
 
-            if(taskParams.parentFolder){
-                destPath += taskParams.parentFolder + "/"
-                relativeXmlPath += taskParams.parentFolder + "/";
-            }
+        //path to return for the file
+        var srcPath = config.fileStore.resourceFolder + srcPath;
+        var destPath = this.getStepXMLFolderPath(taskParams.taskId, taskParams.stepIndex);
+        var relativeXmlPath = this.getSimsXmlStepFolderPath(taskParams.taskId, taskParams.stepIndex);
 
-            destPath += fileName;
-        
-         return new Promise(function(resolve, reject){
+        if (taskParams.parentFolder) {
+            destPath += taskParams.parentFolder + "/"
+            relativeXmlPath += taskParams.parentFolder + "/";
+        }
+
+        destPath += fileName;
+
+        return new Promise(function (resolve, reject) {
 
             fse.copy(srcPath, destPath, function (error) {
                 if (!error) {
-                    var resolveParam = {"filePath" : relativeXmlPath + fileName, "fileType" : resFileType};
+                    var resolveParam = { "filePath": relativeXmlPath + fileName, "fileType": resFileType };
                     resolve(resolveParam);
                 }
                 else {
                     reject(error);
                 }
             });
-        
+
         });
     }
 
 
     readTaskRes(filepath) {
         var absolutePath = config.fileStore.resourceFolder + filepath;
-        return new Promise(function(resolve,reject) {
+        return new Promise(function (resolve, reject) {
             fs.readFile(absolutePath, 'utf8', function (error, fileData) {
-                if(!error){
-                    var resolveParam = {"fileData":fileData,"filePath":absolutePath};
+                if (!error) {
+                    var resolveParam = { "fileData": fileData, "filePath": absolutePath };
                     resolve(resolveParam);
                 }
-                else{
+                else {
                     reject(error);
                 }
             });
         });
-    }    
+    }
     getTaskFolderPath(taskId) {
 
         let taskIdArr = taskId.toLowerCase().split('.');
@@ -126,10 +126,10 @@ class FileStoreController {
     }
 
     removeResourceFile(filePath) {
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve, reject) => {
             filePath = config.fileStore.resourceFolder + filePath;
-            fs.unlink(filePath, (error)=> {
-                if(error) {
+            fs.unlink(filePath, (error) => {
+                if (error) {
                     reject(error);
                 } else {
                     resolve("success");
@@ -139,15 +139,15 @@ class FileStoreController {
     }
 
     getFileFromFileStore(filePath, folder) {
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve, reject) => {
             let absolutePath = filePath;;
-            
-            if(folder) {
+
+            if (folder) {
                 absolutePath = folder + filePath;
             }
 
             fs.readFile(absolutePath, 'utf8', function (error, data) {
-                if(error) {
+                if (error) {
                     reject(error);
                 }
                 else {
@@ -156,7 +156,7 @@ class FileStoreController {
             });
         })
     }
-  
+
     uploadFileHandler() {
         let self = this;
         let storage = multer.diskStorage({
@@ -187,9 +187,9 @@ class FileStoreController {
     }
 
     createFolder(folderPath) {
-        return new Promise((resolve, reject)=> {
+        return new Promise((resolve, reject) => {
             mkdirp(folderPath, (error) => {
-                if(error) {
+                if (error) {
                     reject(error);
                 }
                 else {
@@ -209,23 +209,23 @@ class FileStoreController {
 
     saveFileToFileStore(filepath, fileName, file) {
         return this.createFolder(filepath)
-            .then((success)=> {
+            .then((success) => {
                 fs.writeFile(filepath + fileName, file, (err) => {
-                    if(err) {
+                    if (err) {
                         return Promise.reject(err);
                     }
                     else {
                         return Promise.resolve("saved in directory");
                     }
                 });
-            }, (err)=> {
+            }, (err) => {
                 return Promise.reject(err);
             });
     }
 
-     getResourcePath(filePath, folder){
+    getResourcePath(filePath, folder) {
         return folder + filePath;
-     }
+    }
 }
 
 module.exports.fileStoreController = new FileStoreController();
