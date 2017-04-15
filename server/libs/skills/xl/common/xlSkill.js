060be5ff65a20f7cfd05750d06439b7c5df9d2bf
Taneja, Kapil <Kapil.Taneja@pearson.com>
2017-04-15 10:28:08 +0530

    Committing the changes for the attribute creation with handling the empty file paths

--- server/libs/skills/xl/common/xlSkill.js
+++ server/libs/skills/xl/common/xlSkill.js
@@ -5,19 +5,25 @@ const xmlUtil = require("../../../../utils/xmlUtil");
 
 module.exports = class ExcelBaseSkill extends baseSkill{
 
+      
+
+
     //dynamic sheet changes   
-    init(data, callback) {
-        var self = this;
-        var initDocJSonPath = data.initDocJSonPath;
-        var dbMgr = data.dbMgr;
-        return dbMgr.readFileFromFileStore(initDocJSonPath)
-        .then(function(resolveParam){ 
-            self.initDocJson = JSON.parse(resolveParam.fileData);
-            self.generateSheetNamesMap();
-            return Promise.resolve(true);
-        },function(error){
-            return Promise.reject(error)
-        });
+     init(data) {
+         var self = this;
+         var initDocJSonPath = data.initDocJSonPath;
+       var dbMgr = data.dbMgr;
+       
+       
+       return dbMgr.readFileFromFileStore(initDocJSonPath).then(function(resolveParam){ 
+              
+                self.initDocJson = JSON.parse(resolveParam.fileData);
+                self.generateSheetNamesMap();
+               return Promise.resolve();
+              
+          },function(error){
+              return Promise.reject(error)
+          });
    }
 
    generateSheetNamesMap(){
@@ -40,8 +46,9 @@ module.exports = class ExcelBaseSkill extends baseSkill{
       var taskParams = skillParams.taskParams;
       var paramValueObj = skillParams.paramsObj;
       var finalObject = {};
-      finalObject["folderPath"] = xmlUtil.generateStepFolderPath(taskParams.taskId, taskParams.stepIndex) + this.genImageJsonResFolder(taskParams.stateId);
       skillParams.taskParams.parentFolder = this.genImageJsonResFolder(taskParams.stateId);
+      finalObject["folderPath"] = xmlUtil.generateStepFolderPath(taskParams.taskId, taskParams.stepIndex) + skillParams.taskParams.parentFolder;
+      
       var promiseArr = [];
       var self = this;
       for(var iterator = 0 ; iterator < paramValueObj["sheets"].length; ++iterator){
@@ -53,13 +60,18 @@ module.exports = class ExcelBaseSkill extends baseSkill{
         console.log("folder operation success");
         finalObject["sheetImages"] = [];
         var preloadResArr = [];
-        for(var index = 0; index < resolveParam.length; ++index){
+        for(var index = 0; (index < resolveParam.length) && (resolveParam[index]!=null); ++index){
             finalObject["sheetImages"].push(resolveParam[index].sheetObject);
             for(var iterator = 0; iterator < resolveParam[index].preloadResArr.length; ++iterator){
                 preloadResArr.push(resolveParam[index].preloadResArr[iterator]);
             }
         }
-        finalObject = JSON.stringify(finalObject);
+        if(finalObject["sheetImages"].length == 0){
+            finalObject = "";
+        }else{
+            finalObject = JSON.stringify(finalObject);
+        }
+        
         var resolveParams = {"attrValue":finalObject,"preloadResArr":preloadResArr}
         return Promise.resolve(resolveParams);
       },function(err){
@@ -70,37 +82,52 @@ module.exports = class ExcelBaseSkill extends baseSkill{
 
 
     genSheetPromise(skillParams ,iterator, imageName){
-      var taskParams = skillParams.taskParams;
-      var paramValueObj = skillParams.paramsObj;
-      var self = this;
+      let taskParams = skillParams.taskParams;
+      let paramValueObj = skillParams.paramsObj;
+      let self = this;
       return new Promise(function(resolve,reject){
         
-        var requestArr = [];
+        let requestArr = [];
+        let sheetImgs = {
+            "gridImage":"gridImg",
+            "rowImage":"rowImg",
+            "cellImage":"cellImg",
+            "columnImage":"colImg"
+        };
+        
+        for(let imgName in sheetImgs){
+            requestArr.push(self.genFilePromise(skillParams , iterator, imgName));
+        }
 
-
-        requestArr.push(self.genFilePromise(skillParams , iterator, {"gridImage":"gridImg"}));
-        requestArr.push(self.genFilePromise(skillParams , iterator, {"rowImage":"rowImg"}));
-        requestArr.push(self.genFilePromise(skillParams , iterator, {"cellImage":"cellImg"}));
-        requestArr.push(self.genFilePromise(skillParams , iterator, {"columnImage":"colImg"}));
         Promise.all(requestArr).then(function(resolveParam){
             console.log("single sheet success");
             //update the attrParam for that sheet
-            var resolveObj = {};
-            var preloadResArr = [];
+            let resolveObj = {};
+            let preloadResArr = [];
             for(var index = 0; index < resolveParam.length;++index){
                 var pathKey = Object.keys(resolveParam[index])[0];
                 resolveObj[pathKey] = resolveParam[index][pathKey];
-                preloadResArr.push({"path": resolveObj[pathKey],"type": resolveParam[index]["fileType"] }) 
+                if((resolveObj[pathKey] != "") && (resolveObj[pathKey] != null)){
+                    preloadResArr.push({"path": resolveObj[pathKey],"type": resolveParam[index]["fileType"] }) 
+                }
             }
             
-            var sheetObject = {};
+            let sheetImgPresent = false;
+            let sheetObject = {};
+            
                 sheetObject["sheetNo"] = self.getSheetNumber(paramValueObj["sheets"][iterator].name);
-                sheetObject["gridImg"] = resolveObj["gridImage"].split("/")[resolveObj["gridImage"].split("/").length - 1];
-                sheetObject["rowImg"] = resolveObj["rowImage"].split("/")[resolveObj["rowImage"].split("/").length - 1];
-                sheetObject["cellImg"] = resolveObj["cellImage"].split("/")[resolveObj["cellImage"].split("/").length - 1];
-                sheetObject["colImg"] = resolveObj["columnImage"].split("/")[resolveObj["columnImage"].split("/").length-1];
+                
+                for(let imgName in sheetImgs){
+                    if((resolveObj[imgName] != "") && (resolveObj[imgName] != null)){
+                        sheetObject[sheetImgs[imgName]] = resolveObj[imgName].split("/")[resolveObj[imgName].split("/").length - 1];
+                        sheetImgPresent = true;
+                    }
+                }
 
-            var resolveParam = {"sheetObject":sheetObject,"preloadResArr":preloadResArr};
+            let resolveParam = {"sheetObject":sheetObject,"preloadResArr":preloadResArr};
+            if(sheetImgPresent == false){
+                resolveParam = null;
+            }
             resolve(resolveParam);
         },function(error){
             console.log("single sheet failure");
@@ -110,25 +137,34 @@ module.exports = class ExcelBaseSkill extends baseSkill{
 
     }
 
-    genFilePromise(skillParams ,iterator, imageObj){
+    genFilePromise(skillParams ,iterator, imgType){
         
       var taskParams = skillParams.taskParams;
       var paramValueObj = skillParams.paramsObj;
         
-        return new Promise(function(resolve, reject){
-            var filepath = paramValueObj["sheets"][iterator][Object.keys(imageObj)[0]]["path"];
-            return taskParams.dbFilestoreMgr.copyTaskAssetFile(filepath, taskParams)
-            .then(function(resolveParam){
-                var preloadResArr = [];
-                
-                var resolveParams = {};
-                resolveParams[Object.keys(imageObj)[0]] = resolveParam.filePath;
-                resolveParams["fileType"] = resolveParam.fileType;
-                resolve(resolveParams);
+        var filepath = paramValueObj["sheets"][iterator][imgType]["path"];
+        if(filepath != null && filepath != ""){
+            return new Promise(function(resolve, reject){
+                // var filepath = paramValueObj["sheets"][iterator][Object.keys(imageObj)[0]]["path"];
+                return taskParams.dbFilestoreMgr.copyTaskAssetFile(filepath, taskParams)
+                .then(function(resolveParam){
+                    var preloadResArr = [];
+                    
+                    var resolveParams = {};
+                    resolveParams[imgType] = resolveParam.filePath;
+                    resolveParams["fileType"] = resolveParam.fileType;
+                    resolve(resolveParams);
 
-        },function(error){
-            return Promise.reject(error);
-        })
-    });  
+            },function(error){
+                return Promise.reject(error);
+            })
+        }); //promise object creation end 
+    }// ending the if check
+    else{
+         var resolveParams = {};
+         resolveParams[imgType]  = "";
+         resolveParams["fileType"] = "";
+        return Promise.resolve(resolveParams);
+    }
   }
 }
