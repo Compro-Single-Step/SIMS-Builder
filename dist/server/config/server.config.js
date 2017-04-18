e58f8847cbd8035c0a64a109e07597ced80fda89
vbishg1 <gautam.bishwas@pearson.com>
2017-04-18 12:37:27 +0530

    simbuilder-release publish: vnull

--- dist/server/config/server.config.js
+++
@@ -1,9 +0,0 @@
-var server = {
-    SECRET: 'SIMBUILDER_SECRET',
-    previewServerURL: 'http://dev2.comprotechnologies.com/SimBuilderPreview',
-    TOKEN: {
-        EXPIRATION_TIME: 60 * 60 // in seconds.
-    }
-};
-
-module.exports = server;
--- dist/server/utils/resourceUtil.js
+++
@@ -1,5 +0,0 @@
-module.exports = class ResourceUtil {
-    static getUploadResourceFolderRelativePath(taskId, stepIndex) {
-        return taskId + "/" + stepIndex + "/";
-    }
-};
