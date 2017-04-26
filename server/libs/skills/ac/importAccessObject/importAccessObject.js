a2c0e95495699abf279e3072648f4b8efe2aa97a
Gautam Bishwas <gautam.bishwas@comprotechnologies.com>
2017-04-26 12:49:09 +0530

    Initial Commit

--- server/libs/skills/ac/importAccessObject/importAccessObject.js
+++ server/libs/skills/ac/importAccessObject/importAccessObject.js
@@ -6,12 +6,13 @@ const xmlUtil = require("../../../../utils/xmlUtil");
 class importAccessobject extends baseSkill {
 
   //tooltip text to be displayed
-  getTootlTipText(skillParams, callback){
+  getTootlTipText(skillParams){
     var paramValueObj = skillParams.paramsObj
     var resolveParams = {"Access - " : paramValueObj["DocTitle"]};
     return Promise.resolve(resolveParams);
   }
-  getFileName(skillParams, callback){
+  getFileName(skillParams){
+    debugger;
     var paramValueObj = skillParams.paramsObj;
     var res = paramValueObj.split("\\");
 
@@ -19,145 +20,145 @@ class importAccessobject extends baseSkill {
 
 
    //init DOC JSON 
-  createJsonPath(skillParams, callback) {
+  // createJsonPath(skillParams, callback) {
 
-    var taskParams = skillParams.taskParams;
-    var paramValueObj = skillParams.paramsObj;
+  //   var taskParams = skillParams.taskParams;
+  //   var paramValueObj = skillParams.paramsObj;
 
   
-      return taskParams.dbFilestoreMgr.copyTaskAssetFile(paramValueObj["docData"], taskParams)
-        .then(function(resolveParam){
-          paramValueObj["docData"] = resolveParam.filePath;
-          var preloadResArr = [];
-          preloadResArr.push({ "path": "" +  resolveParam.filePath, "type": "" + resolveParam.fileType })
-          resolveParam = {"attrValue":paramValueObj["docData"],"preloadResArr":preloadResArr}
-          return Promise.resolve(resolveParam);
-    },function(error){
-      return Promise.reject(error);
-      console.log("rejection at the movecellcontent");
-    }).catch(function(error){
-        return Promise.reject(error);
-    });
+  //     return taskParams.dbFilestoreMgr.copyTaskAssetFile(paramValueObj["docData"], taskParams)
+  //       .then(function(resolveParam){
+  //         paramValueObj["docData"] = resolveParam.filePath;
+  //         var preloadResArr = [];
+  //         preloadResArr.push({ "path": "" +  resolveParam.filePath, "type": "" + resolveParam.fileType })
+  //         resolveParam = {"attrValue":paramValueObj["docData"],"preloadResArr":preloadResArr}
+  //         return Promise.resolve(resolveParam);
+  //   },function(error){
+  //     return Promise.reject(error);
+  //     console.log("rejection at the movecellcontent");
+  //   }).catch(function(error){
+  //       return Promise.reject(error);
+  //   });
     
-  }
+  // }
 
-  getSelectedCell(skillParams, callback) {
+  // getSelectedCell(skillParams, callback) {
 
-    var paramValueObj = skillParams.paramsObj
-    var resolveParams = {"attrValue" : paramValueObj["srcRange"]};
-    return Promise.resolve(resolveParams);
+  //   var paramValueObj = skillParams.paramsObj
+  //   var resolveParams = {"attrValue" : paramValueObj["srcRange"]};
+  //   return Promise.resolve(resolveParams);
 
-  }
+  // }
 
-  getSelDragCell(skillParams, callback) {
+  // getSelDragCell(skillParams, callback) {
 
-    var paramValueObj = skillParams.paramsObj
-    //requires sheet name using init doc json
-    var finalObject = {};
-    finalObject["sheetNo"] = this.getSheetNumber(paramValueObj.sheetAction);
-    finalObject["startRange"] = paramValueObj["srcRange"];
-    finalObject["endRange"] = paramValueObj["destRange"];
-    finalObject = JSON.stringify(finalObject);
-    var resolveParams = {"attrValue" :finalObject};
-    return Promise.resolve(resolveParams);
-  }
+  //   var paramValueObj = skillParams.paramsObj
+  //   //requires sheet name using init doc json
+  //   var finalObject = {};
+  //   finalObject["sheetNo"] = this.getSheetNumber(paramValueObj.sheetAction);
+  //   finalObject["startRange"] = paramValueObj["srcRange"];
+  //   finalObject["endRange"] = paramValueObj["destRange"];
+  //   finalObject = JSON.stringify(finalObject);
+  //   var resolveParams = {"attrValue" :finalObject};
+  //   return Promise.resolve(resolveParams);
+  // }
 
-  createHighlightJson(skillParams, callback) {
+  // createHighlightJson(skillParams, callback) {
 
-    var paramValueObj = skillParams.paramsObj
-    // requires sheet number using Init Doc json
-    var finalObject = {};
-    finalObject["sheetNo"] = this.getSheetNumber(paramValueObj.sheetAction);
-    finalObject["range"] = paramValueObj["srcRange"];
-    finalObject = JSON.stringify(finalObject);
-    var resolveParams = {"attrValue" :finalObject};
-    return Promise.resolve(resolveParams);
+  //   var paramValueObj = skillParams.paramsObj
+  //   // requires sheet number using Init Doc json
+  //   var finalObject = {};
+  //   finalObject["sheetNo"] = this.getSheetNumber(paramValueObj.sheetAction);
+  //   finalObject["range"] = paramValueObj["srcRange"];
+  //   finalObject = JSON.stringify(finalObject);
+  //   var resolveParams = {"attrValue" :finalObject};
+  //   return Promise.resolve(resolveParams);
 
-  }
-
-
-  createSheetCellData(skillParams, callback) {
-
-    var taskParams = skillParams.taskParams;
-    var paramValueObj = skillParams.paramsObj;
-    var finalObject = {};
-    finalObject["sheetNo"] = this.getSheetNumber(paramValueObj.sheetAction);
-      return taskParams.dbFilestoreMgr.copyTaskAssetFile(paramValueObj["wbData"], taskParams)
-      .then(function(resolaveParams){
-        paramValueObj["wbData"] = resolaveParams.filePath
-        finalObject["dataJSONPath"] = paramValueObj["wbData"];
-        finalObject = JSON.stringify(finalObject);
-        var preloadResArr = [];
-        preloadResArr.push({ "path": "" + resolaveParams.filePath, "type": "" + resolaveParams.fileType })
-        var resolveParams = {"attrValue": finalObject, "preloadResArr":preloadResArr};
-        return Promise.resolve(resolveParams);
-
-    },function(error){
-        return Promise.reject(error);
-    });
-  }
-
-  getSelectedCellFinal(skillParams, callback) {
-
-    var paramValueObj = skillParams.paramsObj
-    var finalArray = [];
-
-    var valuearray = paramValueObj["destRange"].split(":");
-    valuearray[0].trim();
-    valuearray[1].trim();
-
-    var col1 = valuearray[0].toUpperCase().charAt(0);
-    var col2 = valuearray[1].toUpperCase().charAt(0);
-    var row1 = parseInt(valuearray[0].substring(1, valuearray[0].length));
-    var row2 = parseInt(valuearray[1].substring(1, valuearray[0].length));
-
-    finalArray.push(valuearray[0]);
-
-    for (var iterator = 0; iterator <= col2.charCodeAt(0) - col1.charCodeAt(0); ++iterator) {
-      for (var index = 0; index <= row2 - row1; ++index) {
-        if (iterator != 0 || index != 0)
-          finalArray.push(col1 + row1 + ":" + (String.fromCharCode(col1.charCodeAt(0) + iterator)) + (row1 + index));
-      }
-
-    }
-    var resolveParams = {"attrValue" : finalArray};
-    return Promise.resolve(resolveParams);
+  // }
 
 
-  }
+  // createSheetCellData(skillParams, callback) {
+
+  //   var taskParams = skillParams.taskParams;
+  //   var paramValueObj = skillParams.paramsObj;
+  //   var finalObject = {};
+  //   finalObject["sheetNo"] = this.getSheetNumber(paramValueObj.sheetAction);
+  //     return taskParams.dbFilestoreMgr.copyTaskAssetFile(paramValueObj["wbData"], taskParams)
+  //     .then(function(resolaveParams){
+  //       paramValueObj["wbData"] = resolaveParams.filePath
+  //       finalObject["dataJSONPath"] = paramValueObj["wbData"];
+  //       finalObject = JSON.stringify(finalObject);
+  //       var preloadResArr = [];
+  //       preloadResArr.push({ "path": "" + resolaveParams.filePath, "type": "" + resolaveParams.fileType })
+  //       var resolveParams = {"attrValue": finalObject, "preloadResArr":preloadResArr};
+  //       return Promise.resolve(resolveParams);
+
+  //   },function(error){
+  //       return Promise.reject(error);
+  //   });
+  // }
+
+  // getSelectedCellFinal(skillParams, callback) {
+
+  //   var paramValueObj = skillParams.paramsObj
+  //   var finalArray = [];
+
+  //   var valuearray = paramValueObj["destRange"].split(":");
+  //   valuearray[0].trim();
+  //   valuearray[1].trim();
+
+  //   var col1 = valuearray[0].toUpperCase().charAt(0);
+  //   var col2 = valuearray[1].toUpperCase().charAt(0);
+  //   var row1 = parseInt(valuearray[0].substring(1, valuearray[0].length));
+  //   var row2 = parseInt(valuearray[1].substring(1, valuearray[0].length));
+
+  //   finalArray.push(valuearray[0]);
+
+  //   for (var iterator = 0; iterator <= col2.charCodeAt(0) - col1.charCodeAt(0); ++iterator) {
+  //     for (var index = 0; index <= row2 - row1; ++index) {
+  //       if (iterator != 0 || index != 0)
+  //         finalArray.push(col1 + row1 + ":" + (String.fromCharCode(col1.charCodeAt(0) + iterator)) + (row1 + index));
+  //     }
+
+  //   }
+  //   var resolveParams = {"attrValue" : finalArray};
+  //   return Promise.resolve(resolveParams);
+
+
+  // }
 
 
 
-  getSheetNameAndSheetCountFromInitDocJSON(initDocJSON, dependantSheetArrayInModel) {
+  // getSheetNameAndSheetCountFromInitDocJSON(initDocJSON, dependantSheetArrayInModel) {
 
-    //Add The Required Number of Sheets in Model
-    if (initDocJSON.sheetCount >= dependantSheetArrayInModel.length) {
-      let sheetCountDiff = initDocJSON.sheetCount - dependantSheetArrayInModel.length;
-      while (sheetCountDiff > 0) {
-        dependantSheetArrayInModel.push(JSON.parse(JSON.stringify(dependantSheetArrayInModel[(dependantSheetArrayInModel.length - 1)])));
-        sheetCountDiff--;
-      }
-    }
+  //   //Add The Required Number of Sheets in Model
+  //   if (initDocJSON.sheetCount >= dependantSheetArrayInModel.length) {
+  //     let sheetCountDiff = initDocJSON.sheetCount - dependantSheetArrayInModel.length;
+  //     while (sheetCountDiff > 0) {
+  //       dependantSheetArrayInModel.push(JSON.parse(JSON.stringify(dependantSheetArrayInModel[(dependantSheetArrayInModel.length - 1)])));
+  //       sheetCountDiff--;
+  //     }
+  //   }
 
-    //Add Sheet Names From Init Doc JSON
-    for (let sheetNum = 0; sheetNum < initDocJSON.sheetCount; sheetNum++) {
-      dependantSheetArrayInModel[sheetNum].name = initDocJSON.sheets[sheetNum].name;
-    }
-  }
+  //   //Add Sheet Names From Init Doc JSON
+  //   for (let sheetNum = 0; sheetNum < initDocJSON.sheetCount; sheetNum++) {
+  //     dependantSheetArrayInModel[sheetNum].name = initDocJSON.sheets[sheetNum].name;
+  //   }
+  // }
 
-  addSheetNamesToDropdown(initDocJSON, dependantSheetArrayInModel) {
+  // addSheetNamesToDropdown(initDocJSON, dependantSheetArrayInModel) {
 
-    //Empty the existing array
-    while(dependantSheetArrayInModel.length > 0) {
-      dependantSheetArrayInModel.pop(); //https://jsperf.com/array-clear-methods/3
-    }
-    //Add Sheet Names to Array From Init Doc JSON
-    for (let sheetNum = 0; sheetNum < initDocJSON.sheetCount; sheetNum++) {
-      dependantSheetArrayInModel.push(initDocJSON.sheets[sheetNum].name);
-    }
-  }
-  updateSheetNameUsingDropdown(selectedSheetName, dependentSheetNameInModel) {
-        dependentSheetNameInModel.name = selectedSheetName;
-  }
+  //   //Empty the existing array
+  //   while(dependantSheetArrayInModel.length > 0) {
+  //     dependantSheetArrayInModel.pop(); //https://jsperf.com/array-clear-methods/3
+  //   }
+  //   //Add Sheet Names to Array From Init Doc JSON
+  //   for (let sheetNum = 0; sheetNum < initDocJSON.sheetCount; sheetNum++) {
+  //     dependantSheetArrayInModel.push(initDocJSON.sheets[sheetNum].name);
+  //   }
+  // }
+  // updateSheetNameUsingDropdown(selectedSheetName, dependentSheetNameInModel) {
+  //       dependentSheetNameInModel.name = selectedSheetName;
+  // }
 }
 module.exports = moveCellContent;
