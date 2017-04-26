bf9341a866420d44fd751ba3ba78e4c1dd731ef2
Aman Arora <aman.arora@comprotechnologies.com>
2017-04-26 18:24:23 +0530

    moved enum to switch component file

--- client/app/shared/switch/switch.component.ts
+++ client/app/shared/switch/switch.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '../base.component';
 import { itemSchema } from '../UIConfig.model';
-import { LabelTypes, SwitchLayoutModes, SwitchValueOptionType } from '../enums';
+import { LabelTypes } from '../enums';
 
 @Component({
   selector: 'app-switch',
@@ -25,19 +25,29 @@ export class SwitchComponent extends BaseComponent {
     this.labelConfig.rendererProperties.type = LabelTypes.ELEMENT_HEADING;
     this.itemList = this.compConfig.items;
     this.modelRef = this.builderModelSrvc.getStateRef(this.compConfig.val);
-    this.layout_mode = SwitchLayoutModes[this.compConfig.rendererProperties.layout_mode]
-    this.valueOptions = this.getValueOption(SwitchValueOptionType[this.compConfig.rendererProperties.value_type]);
+    this.layout_mode = LayoutModes[this.compConfig.rendererProperties.layout_mode]
+    this.valueOptions = this.getValueOptions(ValueOptionType[this.compConfig.rendererProperties.value_type]);
   }
 
   selectedItemChange() {
     this.emitEvents(this.modelRef["value"]);
   }
 
-  getValueOption (value_type) {
-    return {
-      "BOOLEAN": ["True", "False"],
-      "QUESTION": ["Yes", "No"],
-      "SWITCH": ["On", "Off"]
-    }[value_type];
+  getValueOptions (value_type) {
+    let options = {};
+    options[ValueOptionType.BOOLEAN] = ["True", "False"];
+    options[ValueOptionType.QUESTION] = ["Yes", "No"];
+    options[ValueOptionType.SWITCH] = ["On", "Off"];
+    return options[ValueOptionType[value_type]];
   }
 }
+enum LayoutModes {
+  RIGHT = <any>"right",
+  BELOW = <any>"below"
+}
+enum ValueOptionType {
+  BOOLEAN = <any>"boolean",
+  QUESTION = <any>"question",
+  SWITCH = <any>"switch"
+}
+
