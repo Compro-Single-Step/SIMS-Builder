731d4da5b80d6198e3c7e7380ca5ecb3f1473713
ravicompro <ravi.sahu@comprotechnologies.com>
2017-05-05 13:37:02 +0530

    updating code.

--- server/libs/skills/wd/common/wdSkill.js
+++ server/libs/skills/wd/common/wdSkill.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const BaseSkill = require("../../common/baseSkill");
 const config = require('../../../../config/config');
 const findInFiles = require('find-in-files');
+const replace = require('replace-in-file');
 //Wrod based Common Functionality goes here 
 
 const xmlUtil = require("../../../../utils/xmlUtil");
@@ -31,15 +32,10 @@ module.exports = class WordSkill extends BaseSkill {
             let fromArray = [];
             let toArray = [];
 
-            findInFiles
-
-            return fs.readFileAsync(absolutePath, "utf8")
-                .then((htmlResource) => {
-                    let doc = new DOMParser().parseFromString(htmlResource);
-                    let imgTagArray = doc.getElementsByTagName('img');
-
-                    for (let i = 0; i < imgTagArray.length; i++) {
-                        let imageName = imgTagArray[i].getAttribute('approot');
+            return findInFiles.find(/^\{#(.*)#}$/g, absolutePath)
+                .then((imgNameArray) => {
+                    for (let i = 0; i < imgNameArray.length; i++) {
+                        let imageName = imgNameArray[i];
 
                         docImages.forEach(function (imgObject) {
                             if (imageName === imgObject.displayName) {
@@ -49,14 +45,47 @@ module.exports = class WordSkill extends BaseSkill {
                                 toArray.push('{#approot#}/Assets/' + imageName);
                             }
                         });
+                    }
+
+                    let options = {
+                        files: 'absolutePath',
+                        from: fromArray,
+                        to: toArray,
+                        encoding: 'utf8'
                     };
 
-                    return fs.writeFileAsync(absolutePath, new XMLSerializer().serializeToString(doc));
-                }).then((data) => {
+                    return replace(options);
+                }).then((changedFiles) => {
+                    console.log('Modified files:', changedFiles.join(', '));
                     return Promise.resolve({ "attrValue": resourcePath });
                 }).catch((error) => {
                     return Promise.reject(error);
-                });
+                });;
+
+            // return fs.readFileAsync(absolutePath, "utf8")
+            //     .then((htmlResource) => {
+            //         let doc = new DOMParser().parseFromString(htmlResource);
+            //         let imgTagArray = doc.getElementsByTagName('img');
+
+            //         for (let i = 0; i < imgTagArray.length; i++) {
+            //             let imageName = imgTagArray[i].getAttribute('approot');
+
+            //             docImages.forEach(function (imgObject) {
+            //                 if (imageName === imgObject.displayName) {
+            //                     fromArray.push(new RegExp('imageName', 'g'));
+            //                     let imagePathArray = imgObject.path.split('/');
+            //                     imageName = imagePathArray[imagePathArray.length - 1];
+            //                     toArray.push('{#approot#}/Assets/' + imageName);
+            //                 }
+            //             });
+            //         };
+
+            //         return fs.writeFileAsync(absolutePath, new XMLSerializer().serializeToString(doc));
+            //     }).then((data) => {
+            //         return Promise.resolve({ "attrValue": resourcePath });
+            //     }).catch((error) => {
+            //         return Promise.reject(error);
+            //     });
 
         } catch (error) {
             return Promise.reject(error);
