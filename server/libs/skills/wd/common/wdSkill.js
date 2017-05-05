fae16c9232ecc0eec0d087e7a94cb3a662f342c5
ravicompro <ravi.sahu@comprotechnologies.com>
2017-05-05 16:05:08 +0530

    updating code to replace image reference.

--- server/libs/skills/wd/common/wdSkill.js
+++ server/libs/skills/wd/common/wdSkill.js
@@ -1,8 +1,8 @@
 const xmldom = require('xmldom');
 const DOMParser = xmldom.DOMParser;
 const XMLSerializer = xmldom.XMLSerializer;
-//const Promise = require('bluebird');
-const fs = require('fs');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 const BaseSkill = require("../../common/baseSkill");
 const config = require('../../../../config/config');
 const findInFiles = require('find-in-files');
@@ -25,21 +25,26 @@ module.exports = class WordSkill extends BaseSkill {
 
     resourcePathWithImages(skillParams) {
         try {
-            let paramValueObj = skillParams.paramsObj;
-            let resourcePath = paramValueObj["resourcePath"];
-            let docImages = paramValueObj["docImages"];
+            let { resourcePath, docImages } = skillParams.paramsObj;
             let absolutePath = config.fileStore.resourceFolder + resourcePath;
             let fromArray = [];
             let toArray = [];
 
-            return findInFiles.find(/^\{#(.*)#}$/g, absolutePath)
-                .then((imgNameArray) => {
+            return fs.readFileAsync(absolutePath, 'utf8')
+                .then((htmlResource) => {
+                    var regex = /{#([^}]*)#}/g;
+                    let matches,
+                        imgNameArray = [];
+                    while (matches = regex.exec(htmlResource)) {
+                        imgNameArray.push(matches[1]);
+                    }
+
                     for (let i = 0; i < imgNameArray.length; i++) {
                         let imageName = imgNameArray[i];
 
                         docImages.forEach(function (imgObject) {
                             if (imageName === imgObject.displayName) {
-                                fromArray.push(new RegExp('imageName', 'g'));
+                                fromArray.push(new RegExp(`{#${imageName}#}`, 'g'));
                                 let imagePathArray = imgObject.path.split('/');
                                 imageName = imagePathArray[imagePathArray.length - 1];
                                 toArray.push('{#approot#}/Assets/' + imageName);
@@ -48,7 +53,7 @@ module.exports = class WordSkill extends BaseSkill {
                     }
 
                     let options = {
-                        files: 'absolutePath',
+                        files: absolutePath,
                         from: fromArray,
                         to: toArray,
                         encoding: 'utf8'
@@ -56,37 +61,10 @@ module.exports = class WordSkill extends BaseSkill {
 
                     return replace(options);
                 }).then((changedFiles) => {
-                    console.log('Modified files:', changedFiles.join(', '));
                     return Promise.resolve({ "attrValue": resourcePath });
                 }).catch((error) => {
                     return Promise.reject(error);
-                });;
-
-            // return fs.readFileAsync(absolutePath, "utf8")
-            //     .then((htmlResource) => {
-            //         let doc = new DOMParser().parseFromString(htmlResource);
-            //         let imgTagArray = doc.getElementsByTagName('img');
-
-            //         for (let i = 0; i < imgTagArray.length; i++) {
-            //             let imageName = imgTagArray[i].getAttribute('approot');
-
-            //             docImages.forEach(function (imgObject) {
-            //                 if (imageName === imgObject.displayName) {
-            //                     fromArray.push(new RegExp('imageName', 'g'));
-            //                     let imagePathArray = imgObject.path.split('/');
-            //                     imageName = imagePathArray[imagePathArray.length - 1];
-            //                     toArray.push('{#approot#}/Assets/' + imageName);
-            //                 }
-            //             });
-            //         };
-
-            //         return fs.writeFileAsync(absolutePath, new XMLSerializer().serializeToString(doc));
-            //     }).then((data) => {
-            //         return Promise.resolve({ "attrValue": resourcePath });
-            //     }).catch((error) => {
-            //         return Promise.reject(error);
-            //     });
-
+                });
         } catch (error) {
             return Promise.reject(error);
         }
