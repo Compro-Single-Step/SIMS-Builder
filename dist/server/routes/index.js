eb0b3b9117eb1b14e234c7e2a3c8c54be01cba37
tanujaggarwal <tanuj.aggarwal@comprotechnologies.com>
2017-05-11 17:02:35 +0530

    test with skill route remove

--- dist/server/routes/index.js
+++ dist/server/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var loginRouter = require('./login/login.route');
 var taskBuilderData = require('./taskBuilderData');
-var skillRoutes = require('./skill.routes');
 var verifyToken = require('./login/verifyToken');
 var userData = require('./userData');
 var taskPreviewRouter = require('./taskpreview/taskPreview');
@@ -13,7 +12,6 @@ module.exports = function () {
 	router.use('*', verifyToken);
 
 	router.use('/taskPreview', taskPreviewRouter());
-	router.use('/skill', skillRoutes);
 	router.use('/fetchTaskData', taskBuilderData);
 	router.use('/user', userData);
 
