944b5fe08843ff50f120bce6025dcc44742cfabd
tanujaggarwal <tanuj.aggarwal@comprotechnologies.com>
2017-05-12 10:06:31 +0530

    Revert "converting to es5"
    This reverts commit d175d4eb33a300d0fe7e621bb9f7254ab6a95569.

--- dist/server/modules/skill/xmlGenerationHandler.js
+++ dist/server/modules/skill/xmlGenerationHandler.js
@@ -1,6 +1,6 @@
-var dbFilestoreMgr = require('./dbFilestoreMgr');
-var translator = require("./ioTranslator.js");
-var XmlGenerator = require("./xmlGenerator/stepXMLGenerator");
+const dbFilestoreMgr = require('./dbFilestoreMgr');
+const XmlGenerator = require("./xmlGenerator/stepXMLGenerator");
+
 var mapTranslationParams = function (IOMap, stepUIState, skillRef, taskId, stepIndex, dbFilestoreMgr) {
     this.IOMap = IOMap;
     this.stepUIState = stepUIState;
@@ -9,37 +9,33 @@ var mapTranslationParams = function (IOMap, stepUIState, skillRef, taskId, stepI
     this.stepIndex = stepIndex;
     this.dbFilestoreMgr = dbFilestoreMgr;
 };
+
 module.exports.generateStepXML = function (templateId, taskId, stepIndex, stepText, skillRef) {
-    return Promise.all([dbFilestoreMgr.getStepUIState(taskId, stepIndex), dbFilestoreMgr.getIOMap(templateId)]).then(function (_a) {
-        var stepUIState = _a[0], IOMapJson = _a[1];
+
+    return Promise.all([dbFilestoreMgr.getStepUIState(taskId, stepIndex), dbFilestoreMgr.getIOMap(templateId)]).then(([stepUIState, IOMapJson]) => {
         //IO Translator
-        var IOMap = JSON.parse(IOMapJson);
-        var mapTranslationParam = new mapTranslationParams(IOMap, stepUIState, skillRef, taskId, stepIndex, dbFilestoreMgr);
+        let IOMap = JSON.parse(IOMapJson);
+        let mapTranslationParam = new mapTranslationParams(IOMap, stepUIState, skillRef, taskId, stepIndex, dbFilestoreMgr);
         // let attrValueMap = translator.getAttrValueMap(IOMap, stepUIState, skillRef, taskId, stepIndex,dbFilestoreMgr);
-        return translator.getAttrValueMap(mapTranslationParam).then(function (_a) {
-            var attrValMap = _a[0], copyResPromiseArray = _a[1];
+        return translator.getAttrValueMap(mapTranslationParam).then(([attrValMap, copyResPromiseArray]) => {
             //XML generation
-            return Promise.all([dbFilestoreMgr.getSkillXML(templateId).then(function (skillTemplate) {
-                    var xmlGenerator = new XmlGenerator();
-                    var OutputXML = xmlGenerator.generateXml(skillTemplate, attrValMap, stepText);
-                    //Saving Step XML in File Store
-                    return dbFilestoreMgr.saveStepXML(taskId, stepIndex, OutputXML).then(function (msg) {
-                        return Promise.resolve(msg);
-                    }, function (error) {
-                        return Promise.reject(error);
-                    });
-                }, function (error) {
+            return dbFilestoreMgr.getSkillXML(templateId).then(skillTemplate => {
+                let xmlGenerator = new XmlGenerator();
+                let OutputXML = xmlGenerator.generateXml(skillTemplate, attrValMap, stepText);
+
+                //Saving Step XML in File Store
+                return dbFilestoreMgr.saveStepXML(taskId, stepIndex, OutputXML).then(msg => {
+                    return Promise.resolve(msg);
+                }, error => {
                     return Promise.reject(error);
-                })].concat(copyResPromiseArray)).then(function (_a) {
-                var msg = _a[0], copyResMsg = _a.slice(1);
-                return Promise.resolve(msg);
-            })["catch"](function (error) {
+                });
+            }, error => {
                 return Promise.reject(error);
-            });
-        }, function (error) {
+            })
+        }, error => {
             return Promise.reject(error);
         });
-    }, function (error) {
+    }, error => {
         return Promise.reject(error);
     });
 };
