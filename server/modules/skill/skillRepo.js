d93984d4acd3849c21d5f261b3480e08d7abb07e
tanujaggarwal <tanuj.aggarwal@comprotechnologies.com>
2017-05-13 12:16:09 +0530

    Update skillRepo.js

--- server/modules/skill/skillRepo.js
+++ server/modules/skill/skillRepo.js
@@ -75,7 +75,7 @@ module.exports = {
         ]
     },
     "insertaudio": {
-        primaryFile: "pp/insertAudio/insertAudio",
+        primaryFile: "pp/InsertAudio/insertAudio",
         dependencyFiles: [
             {
                 primaryFile: "pp/common/ppSkill",
