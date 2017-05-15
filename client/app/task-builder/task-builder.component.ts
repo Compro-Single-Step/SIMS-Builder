2c248046ad6347ad87f33f654ac09e979de0d6a8
Taneja, Kapil <Kapil.Taneja@pearson.com>
2017-05-15 13:49:47 +0530

    Null value bugfix v2

--- client/app/task-builder/task-builder.component.ts
+++ client/app/task-builder/task-builder.component.ts
@@ -22,6 +22,7 @@ export class TaskBuilderComponent implements OnInit {
  SelectedTemplate="";
  SelectedStep ;
  selectedTemplateOption="";
+ disableOkBtn = true;
  @ViewChild('selectTemplateDialog') public SelectTemplateDialog:ModalDirective;
  constructor(private route: ActivatedRoute,private previewService:PreviewService ,private taskDataService:TaskDataService, 
     private router: Router) { 
@@ -35,6 +36,7 @@ export class TaskBuilderComponent implements OnInit {
 	// 	});
  }
  selectTemplate($event,selectedTemplate){
+	 this.disableOkBtn = false;
 	 jQuery($event.currentTarget).addClass("selected").siblings().removeClass("selected");
 	 this.selectedTemplateOption = selectedTemplate;
  }
@@ -72,9 +74,13 @@ initialiseTaskData() {
 	}
 	stepNavigationListner(steptemplate,stepIndex){
 		this.SelectedStep = this.TaskData["stepData"][stepIndex];
-		if(steptemplate!= "NotSelected")
+		if (steptemplate != "NotSelected" && steptemplate != null) {
 			this.router.navigate(["task",this.TaskData["id"],"step",this.SelectedStep.Index,"template",steptemplate]);
+		}
 		else{
+			this.selectedTemplateOption = "";
+			jQuery(".list-group-item").removeClass("selected");
+			this.disableOkBtn = true;
 			this.SelectTemplateDialog.show();
 		}
 	}
@@ -94,7 +100,12 @@ initialiseTaskData() {
 				});
 	}
 	callSetTemplate(){
-		this.setTempalateMap(this.selectedTemplateOption);
+		if (this.selectedTemplateOption != "") {
+			this.setTempalateMap(this.selectedTemplateOption);
+		}
+		else {
+			this.displayMessage("Please select a template");
+		}
 	}
 	displayMessage(messageText){
 		Messenger.options = { extraClasses: 'messenger-fixed messenger-on-top',
