40d9d37a862fc626496a8c3879ff274fc34f5b94
Punit Gupta <punit.gupta@comprotechnologies.com>
2017-05-20 14:17:48 +0530

    updating files

--- server/libs/skills/pp/CropPicture/CropPicture.js
+++ server/libs/skills/pp/CropPicture/CropPicture.js
@@ -9,14 +9,21 @@ module.exports = class CropPicture extends PPTBaseSkill {
 
     getSubCompHostParam(skillParams) {
         var selectedSlide = skillParams.paramsObj.selectedSlide.data,
-            attrValue = '{"Mode":"ImagePlaceHolder", "HostParams":{"slide":' + selectedSlide + '}}';
+            attrValue = '{"Mode":"ImagePlaceHolder", "HostParams":{"slide":"' + selectedSlide + '"}}';
+        return Promise.resolve({ attrValue });
+    }
+
+    getUpdatedThumbnailData(skillParams) {
+        var selectedSlide = skillParams.paramsObj.selectedSlide.data,
+            slidePath = skillParams.taskParams.addResourceToMap({ "path": skillParams.paramsObj["imgPath"] }).absFilePath,
+            attrValue = `[{"Number":"${selectedSlide}","ThumbHtml":"<img src='${slidePath}'/>"}]`;
         return Promise.resolve({ attrValue });
     }
 
     getUpdatedSlideData(skillParams) {
         var selectedSlide = skillParams.paramsObj.selectedSlide.data,
             slidePath = skillParams.taskParams.addResourceToMap({ "path": skillParams.paramsObj["imgPath"] }).absFilePath,
-            attrValue = `[{"Number":"${selectedSlide}","ThumbHtml":"<img src='${slidePath}'/>"}]`;
+            attrValue = `[{"Number":"${selectedSlide}","Html":"<img src='${slidePath}'/>"}]`;
         return Promise.resolve({ attrValue });
     }
 
