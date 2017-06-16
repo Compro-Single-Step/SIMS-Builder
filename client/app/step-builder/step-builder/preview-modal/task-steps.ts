e5e831689c2030b582af81ed8db38648c9d24d94
ravicompro <ravi.sahu@comprotechnologies.com>
2017-06-16 12:48:15 +0530

    adding task-steps.js

---
+++ client/app/step-builder/step-builder/preview-modal/task-steps.ts
@@ -0,0 +1,31 @@
+export let stepsData: Object = {
+    "1": {
+        "test_template_id": "df380df4-450d-11e7-a919-92ebcb67fe33",
+        "methods": [
+            {
+                "index": "0",
+                "type": "Ribbon"
+            },
+            {
+                "index": "1",
+                "type": "Ribbon"
+            },
+            {
+                "index": "2",
+                "type": "Right - Click"
+            },
+            {
+                "index": "3",
+                "type": "Keyboard"
+            },
+            {
+                "index": "4",
+                "type": "Other"
+            },
+            {
+                "index": "5",
+                "type": "Ribbon"
+            }
+        ]
+    }
+};
