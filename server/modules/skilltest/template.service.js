7afdf1f849e8b443128d26ce20b7dc34c3b0f049
Abhishek Kumar <abhishek.kumar@comprotechnologies.com>
2017-06-23 17:12:22 +0530

    adding route for get pathways by task id

--- server/modules/skilltest/template.service.js
+++ server/modules/skilltest/template.service.js
@@ -63,13 +63,54 @@ class TemplateService {
 
       dao.get(config.dao.linkage, query)
         .then((linkages) => {
-          resolve(linkages);
+          if(linkages.length) {
+            resolve(linkages[0]);
+          } else {
+            reject({});
+          }
         },(error) => {
           reject(error);
         });
 
     });
   }
+
+  getPathwaysByTemplateId(templateId, step_no, appType) {
+
+    return new Promise((resolve, reject) => {
+      this.getTemplateById(templateId, appType)
+        .then((templates) => {
+
+          if(!templates.length) {
+            reject(config.messages.notFound);
+          } else {
+            var _methods = {
+              "pathways": []
+            };
+
+            if (templates[0].items[0].methods.length){
+              for(var i in templates[0].items[0].methods) {
+
+                let _m = {
+                  name: templates[0].items[0].methods[i].type,
+                  [ step_no ] : {
+                    "method": (parseInt(i) + 1),
+                    "type": templates[0].items[0].methods[i].type
+                  }
+                }
+                _methods.pathways.push(_m);
+              }
+            }
+
+            resolve(_methods);
+          }
+        }, (error) => {
+          reject(error);
+        });
+
+    })
+  }
+
 }
 
 module.exports = new TemplateService();
